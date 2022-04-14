1364b86a53ebc4e293eb9542fe96b713ed8d9502
WilliamLiuACPC <LiuSiming220036@163.com>
2022-04-14 13:51:29 -0700

    basic elements defined

--- include/hdrplus/finish.h
+++ include/hdrplus/finish.h
@@ -1,13 +1,38 @@
 #pragma once
 
 #include <opencv2/opencv.hpp> // all opencv header
+#include <string>
 
 namespace hdrplus
 {
 
 class finish
 {
+    // public:
+    //     cv::Mat mergedBayer; // merged image from Merge Module
+    //     std::string burstPath; // path to burst images
+    //     std::string[] rawPathList; // a list or array of the path to all burst imgs under burst Path
+    //     int refIdx; // index of the reference img
 
+
+    //     finish(string burstPath, cv::Mat mergedBayer,int refIdx){
+    //         this->refIdx = refIdx;
+    //         this->burstPath = burstPath
+    //         this->mergedBayer = mergedBayer;
+    //     }
+
+    //     finish(string burstPath, std::string mergedBayerPath,int refIdx){
+    //         this->refIdx = refIdx;
+    //         this->burstPath = burstPath
+    //         this->mergedBayer = mergedBayer;
+    //     }
+
+    //     ~finish() = default;
+
+
+    // private:
+
+        
 };
 
 } // namespace hdrplus
