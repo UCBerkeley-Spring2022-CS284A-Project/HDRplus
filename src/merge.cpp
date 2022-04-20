576139d07afdd255b023a898b28771fe02c38813
Haohua-Lyu <haohua@berkeley.edu>
2022-04-20 01:36:33 -0700

    Compute lambda_shot and lambda_read

--- src/merge.cpp
+++ src/merge.cpp
@@ -34,4 +34,23 @@ void merge::process( const hdrplus::burst& burst_images, \
     // cv::imwrite("tile0.jpg", outputImg1);
 }
 
+std::pair<double, double> merge::getNoiseParams( int ISO, \
+                                          int white_level, \
+                                          double black_level ) 
+{
+    // Set ISO to 100 if not positive
+    ISO = ISO <= 0 ? 100 : ISO;
+
+    // Calculate shot noise and read noise parameters w.r.t ISO 100
+    double lambda_shot_p = ISO / 100.0f * baseline_lambda_shot;
+	double lambda_read_p = (ISO / 100.0f) * (ISO / 100.0f) * baseline_lambda_read;
+
+    // Rescale shot and read noise to normal range
+    double lambda_shot = lambda_shot_p * (white_level - black_level);
+    double lambda_read = lambda_read_p * (white_level - black_level) * (white_level - black_level);
+
+    // return pair
+    return std::make_pair(lambda_shot, lambda_read);
+}
+
 } // namespace hdrplus
