89ad353e1aa5a0a1d8b34ba496796a615dc0fe80
Haohua-Lyu <haohua@berkeley.edu>
2022-04-27 04:09:44 -0700

    Modify get noise param accordingly

--- src/merge.cpp
+++ src/merge.cpp
@@ -377,73 +377,7 @@ std::pair<double, double> merge::getNoiseParams( int ISO, \
                                           double black_level ) 
 {
     double lambda_shot, lambda_read;
-    std::tie(lambda_shot, lambda_read) = merge::getNoiseParams(ISO, white_level, black_level);
-
-    
-
-    // cv::Mat outputImg = reference_image.clone();
-    // cv::cvtColor(outputImg, outputImg, cv::COLOR_GRAY2RGB);
-    // cv::imwrite("ref.jpg", outputImg);
-    // cv::Mat outputImg1 = reference_tiles[0].clone();
-    // cv::cvtColor(outputImg1, outputImg1, cv::COLOR_GRAY2RGB);
-    // cv::imwrite("tile0.jpg", outputImg1);
-}
-
-std::pair<double, double> merge::getNoiseParams( int ISO, \
-                                          int white_level, \
-                                          double black_level ) 
-{
-    // Set ISO to 100 if not positive
-    ISO = ISO <= 0 ? 100 : ISO;
-
-    // Calculate shot noise and read noise parameters w.r.t ISO 100
-    double lambda_shot_p = ISO / 100.0f * baseline_lambda_shot;
-	double lambda_read_p = (ISO / 100.0f) * (ISO / 100.0f) * baseline_lambda_read;
-
-    // Rescale shot and read noise to normal range
-    double lambda_shot = lambda_shot_p * (white_level - black_level);
-    double lambda_read = lambda_read_p * (white_level - black_level) * (white_level - black_level);
-
-    // return pair
-    return std::make_pair(lambda_shot, lambda_read);
-}
-
-std::pair<double, double> merge::getNoiseParams( int ISO, \
-                                          int white_level, \
-                                          double black_level ) 
-{
-    // Set ISO to 100 if not positive
-    ISO = ISO <= 0 ? 100 : ISO;
-
-    // Calculate shot noise and read noise parameters w.r.t ISO 100
-    double lambda_shot_p = ISO / 100.0f * baseline_lambda_shot;
-	double lambda_read_p = (ISO / 100.0f) * (ISO / 100.0f) * baseline_lambda_read;
-
-    // Rescale shot and read noise to normal range
-    double lambda_shot = lambda_shot_p * (white_level - black_level);
-    double lambda_read = lambda_read_p * (white_level - black_level) * (white_level - black_level);
-
-    // return pair
-    return std::make_pair(lambda_shot, lambda_read);
-}
-
-std::pair<double, double> merge::getNoiseParams( int ISO, \
-                                          int white_level, \
-                                          double black_level ) 
-{
-    // Set ISO to 100 if not positive
-    ISO = ISO <= 0 ? 100 : ISO;
-
-    // Calculate shot noise and read noise parameters w.r.t ISO 100
-    double lambda_shot_p = ISO / 100.0f * baseline_lambda_shot;
-	double lambda_read_p = (ISO / 100.0f) * (ISO / 100.0f) * baseline_lambda_read;
-
-    // Rescale shot and read noise to normal range
-    double lambda_shot = lambda_shot_p * (white_level - black_level);
-    double lambda_read = lambda_read_p * (white_level - black_level) * (white_level - black_level);
-
-    // return pair
-    return std::make_pair(lambda_shot, lambda_read);
+    std::tie(lambda_shot, lambda_read) = burst_images.bayer_images[burst_images.reference_image_idx].get_noise_params();
 }
 
 } // namespace hdrplus
