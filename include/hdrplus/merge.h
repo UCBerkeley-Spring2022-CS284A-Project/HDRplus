4a20b807f0be0b23b4e1d61b1c1de3eaffbf0be9
Haohua-Lyu <haohua@berkeley.edu>
2022-04-29 03:32:18 -0700

    Implemented actual merging (4.4)

--- include/hdrplus/merge.h
+++ include/hdrplus/merge.h
@@ -5,12 +5,15 @@
 #include <cmath>
 #include "hdrplus/burst.h"
 
+#define TILE_SIZE 16
+
 namespace hdrplus
 {
 
 class merge
 {
     public:
+        int offset = TILE_SIZE / 2;
         float baseline_lambda_shot = 3.24 * pow( 10, -4 );
         float baseline_lambda_read = 4.3 * pow( 10, -6 );
 
@@ -29,7 +32,7 @@ class merge
                       std::vector<std::vector<std::vector<std::pair<int, int>>>>& alignments);
         
     private:
-        cv::Mat cosineWindow1D(cv::Mat input, int window_size = 16) {
+        cv::Mat cosineWindow1D(cv::Mat input, int window_size = TILE_SIZE) {
             cv::Mat output = input.clone();
             for (int i = 0; i < input.cols; ++i) {
                 output.at<float>(0, i) = 1. / 2. - 1. / 2. * cos(2 * M_PI * (input.at<float>(0, i) + 1 / 2.) / window_size);
@@ -55,7 +58,21 @@ class merge
             return window_applied;
         }
 
+        cv::Mat cat2Dtiles(std::vector<std::vector<cv::Mat>> tiles) {
+            std::vector<cv::Mat> rows;
+            for (auto row_tiles : tiles) {
+                cv::Mat row;
+                cv::hconcat(row_tiles, row);
+                rows.push_back(row);
+            }
+            cv::Mat img;
+            cv::vconcat(rows, img);
+            return img;
+        }
+
         std::vector<cv::Mat> getReferenceTiles(cv::Mat reference_image);
+
+        cv::Mat mergeTiles(std::vector<cv::Mat> tiles, int rows, int cols);
 };
 
 } // namespace hdrplus
