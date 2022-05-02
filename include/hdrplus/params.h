de1c8ee1b1a9628aef2252d734480c8837f7a69b
WilliamLiuAtCPC <47164078+WilliamLiuAtCPC@users.noreply.github.com>
2022-05-02 14:21:13 -0700

    Update params.h

--- include/hdrplus/params.h
+++ include/hdrplus/params.h
@@ -20,27 +20,49 @@ class RawpyArgs{
         int output_bps = 16;
 };
 
+class Options{
+    public:
+        std::string input = "";
+        std::string output = "";
+        std::string mode = "full"; //'full' 'align' 'merge' 'finish'
+        int reference = 0;
+        float temporalfactor=75.0;
+        float spatialfactor = 0.1;
+        int ltmGain=-1;
+        double gtmContrast=0.075;
+        int verbose=2; // (0, 1, 2, 3, 4, 5)
+
+};
+
+class Tuning{
+    public:
+        std::string ltmGain = "auto";
+        double gtmContrast = 0.075;
+        std::vector<float> sharpenAmount{1,0.5,0.5};
+        std::vector<float> sharpenSigma{1,2,4};
+        std::vector<float> sharpenThreshold{0.02,0.04,0.06};
+};
+
 class Parameters{
     public:
-        std::string tuning_ltmGain = "auto";
-        double tuning_gtmContrast = 0.075;
-
         std::unordered_map<std::string,bool> flags{
             {"writeReferenceImage",true},
             {"writeGammaReference", true},
-            {"writeMergedImage", false},
+            {"writeMergedImage", true},
             {"writeGammaMerged", true},
-            {"writeShortExposure", false},
-            {"writeLongExposure", false},
-            {"writeFusedExposure", false},
-            {"writeLTMImage", false},
-            {"writeLTMGamma", false},
-            {"writeGTMImage", false},
+            {"writeShortExposure", true},
+            {"writeLongExposure", true},
+            {"writeFusedExposure", true},
+            {"writeLTMImage", true},
+            {"writeLTMGamma", true},
+            {"writeGTMImage", true},
             {"writeReferenceFinal", true},
             {"writeFinalImage", true}
         };
 
         RawpyArgs rawpyArgs;
+        Options options;
+        Tuning tuning;
 
         Parameters()= default;
 
