9b8beb18c00b85fe5899d50038a1c25e0adbc401
WilliamLiuAtCPC <47164078+WilliamLiuAtCPC@users.noreply.github.com>
2022-05-03 13:13:36 -0700

    Update finish.h

--- include/hdrplus/finish.h
+++ include/hdrplus/finish.h
@@ -61,7 +61,7 @@ class finish
         ~finish() = default;
 
         // finish pipeline func
-        void pipeline_finish();
+        void process(std::string burstPath, cv::Mat mergedBayer,int refIdx);
 
         // replace Mat a with Mat b
         void copy_mat_16U(cv::Mat& A, cv::Mat B);
