775aa42c09ebe385328d9e23e8523ab73acf2c22
WilliamLiuAtCPC <47164078+WilliamLiuAtCPC@users.noreply.github.com>
2022-05-06 11:16:40 -0700

    finishing part usable

--- src/hdrplus_pipeline.cpp
+++ src/hdrplus_pipeline.cpp
@@ -8,10 +8,18 @@
 #include "hdrplus/align.h"
 #include "hdrplus/merge.h"
 #include "hdrplus/finish.h"
+#include <fstream>
 
 namespace hdrplus
 {
-    
+void writeCSV(std::string filename, cv::Mat m)
+{
+    std::ofstream myfile;
+    myfile.open(filename.c_str());
+    myfile<< cv::format(m, cv::Formatter::FMT_CSV) << std::endl;
+    myfile.close();
+}
+
 void hdrplus_pipeline::run_pipeline( \
     const std::string& burst_path, \
     const std::string& reference_image_path  )
@@ -25,6 +33,21 @@ void hdrplus_pipeline::run_pipeline( \
 
     // Run merging
     merge_module.process( burst_images, alignments );
+
+    cv::Mat mergedBayer = burst_images.merged_bayer_image.clone();
+
+    std::cout<<std::endl<<"size: "<<mergedBayer.rows<<"*"<<mergedBayer.cols<<std::endl;
+
+    // for(int i=0;i<20;i++){
+    //     u_int16_t* ptr = (u_int16_t*)mergedBayer.data;
+    //     for(int j=0;j<20;j++){
+    //         std::cout<<*(ptr+i*mergedBayer.cols+j)<<", ";
+    //     }
+    //     std::cout<<std::endl;
+    // }
+
+    hdrplus::writeCSV("merged.csv",mergedBayer);
+
 
     // Run finishing
     finish_module.process( burst_path, burst_images.merged_bayer_image, burst_images.reference_image_idx);
